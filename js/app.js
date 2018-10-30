339709b6dd068c42bbfd40466d6a69b880491756
fitsumbeyene <beyeneaddis@yahoo.com>
2018-10-30 11:16:05 -0700

    firstday

--- js/app.js
+++ js/app.js
@@ -3,20 +3,17 @@
 let elBody = document.getElementById('myBody')
  let elBody1= document.getElementById('myBody1')
  let elBody2= document.getElementById("myBody2")
-
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am','1pm', '2pm','3pm','4pm','5pm','6pm','7pm']
 let cookies = []
 
-// this the location of cookies store in Silver-Spring
-
-let Silverspring= {
-location:'Silverspring',
-minNumCus: 20,
-maxNumCus: 100,
-cookiesPerCus:5,
+let maryland = {
+location:'Maryland',
+minNumCus:40,
+maxNumCus:50,
+cookiesPerCus:10,
 cookiesPerHour: function() {
   let randomNumCookies = Math.ceil(Math.random() * (this.maxNumCus-this.minNumCus) + this.minNumCus)
-  // console.log(this.cookiesPerHour.randomNumCookies)
+  
   return randomNumCookies * this.cookiesPerCus
   alert(cookiesPerHour())
 }
@@ -24,9 +21,9 @@ cookiesPerHour: function() {
 
 
 //display store-1 name in h2
-let elSilverspring = document.createElement('h3')
-elBody.appendChild(elSilverspring)
-elSilverspring.innerText = Silverspring.location
+let elmaryland = document.createElement('h3')
+elBody.appendChild(elmaryland)
+elmaryland.innerText = maryland.location
 
 //display store information into html
 // let elList = document.createElement('ul')
@@ -35,17 +32,17 @@ elSilverspring.innerText = Silverspring.location
 
 
 for (let i =0; i<hours.length; i++){
-  console.log(hours[i],'Total # of Cookies/hr in the location of', Silverspring.location, Silverspring.cookiesPerHour())
+  console.log(hours[i],'Total # of Cookies/hr in the location of',maryland.location, maryland.cookiesPerHour())
   let elListItem = document.createElement('ul')
   elBody.appendChild(elListItem)
-  elListItem.innerText = hours[i] + ': ' + Silverspring.cookiesPerHour() 
+  elListItem.innerText = hours[i] + ': ' + maryland.cookiesPerHour() 
 }
 
 // this the location of cookies store in Hyattsville 
 
 
-let Hyattsville= {
-  location:'hyattsville',
+let verginia= {
+  location:'verginia',
   minNumCus: 35,
   maxNumCus: 150,
   cookiesPerCus:15,
@@ -59,9 +56,9 @@ let Hyattsville= {
   
   
   //display store-1 name in h2
-  let elHyattsville = document.createElement('h1')
-  elBody1.appendChild(elHyattsville)
-  elHyattsville.innerText = Hyattsville.location
+  let elverginia= document.createElement('h3')
+  elBody1.appendChild(elverginia)
+  elverginia.innerText = verginia.location
   
   //display store information into html
   // let elList = document.createElement('ul')
@@ -70,16 +67,16 @@ let Hyattsville= {
   
   
   for (let i =0; i<hours.length; i++){
-    console.log(hours[i],'Total # of Cookies/hr in the location of', Hyattsville.location, Hyattsville.cookiesPerHour())
+    console.log(hours[i],'Total # of Cookies/hr in the location of', verginia.location, verginia.cookiesPerHour())
     let elListItem = document.createElement('ul')
     elBody1.appendChild(elListItem)
-    elListItem.innerText = hours[i] + ': ' + Hyattsville.cookiesPerHour() 
+    elListItem.innerText = hours[i] + ': ' + verginia.cookiesPerHour() 
   }
 
   // Store 3 in Greenbelt
 
-  let Greenbelt= {
-    location:'greenbelt',
+  let washington= {
+    location:'washington',
     minNumCus: 22,
     maxNumCus: 120,
     cookiesPerCus:12,
@@ -93,9 +90,9 @@ let Hyattsville= {
     
     
     //display store-1 name in h2
-    let elGreenbelt = document.createElement('h3')
-    elBody2.appendChild(elGreenbelt)
-    elGreenbelt.innerText = Greenbelt.location
+    let elwashington= document.createElement('h3')
+    elBody2.appendChild(elwashington)
+    elwashington.innerText = washington.location
     
     //display store information into html
     // let elList = document.createElement('ul')
@@ -104,9 +101,9 @@ let Hyattsville= {
     
     
     for (let i =0; i<hours.length; i++){
-      console.log(hours[i],'Total # of Cookies/hr in the location of', Greenbelt.location, Greenbelt.cookiesPerHour())
+      console.log(hours[i],'Total # of Cookies/hr in the location of', washington.location, washington.cookiesPerHour())
       let elListItem = document.createElement('ul')
       elBody2.appendChild(elListItem)
-      elListItem.innerText = hours[i] + ': ' + Greenbelt.cookiesPerHour() 
+      elListItem.innerText = hours[i] + ': ' + washington.cookiesPerHour() 
     }
   
